d4fe1aa5e6b95f800ebd33db2f9d56e2e9eaed8d
baskarasakti <baskara.sakti13@gmail.com>
2017-04-19 14:16:25 +0700

    added for view

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -3,29 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Buku extends CI_Controller {
 
+	
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+   * __construct function.
+   * 
+   * @access public
+   * @return void
+   */
+	public function __construct() {
+
+		parent::__construct();
+		$this->load->library(array('session'));
+		$this->load->helper(array('url'));
+		$this->load->model('buku_model');
+
+	}
+
 	public function index()
 	{
+		// create the data object
+	    $data = new stdClass();
+
+	    $buku = $this->buku_model->get_buku_all();
+	    $data = array('buku' => $buku );
+
 		$this->load->library('session');
 		if ($this->session->has_userdata('username')) {
 			$this->load->helper('url');
 			$this->load->view('master/header');
 			$this->load->view('master/navigation');
-			$this->load->view('pages/buku/viewBuku');
+			$this->load->view('pages/buku/viewBuku', $data);
 			$this->load->view('master/jsViewTables');
 			$this->load->view('master/footer');
 		} else {
@@ -36,17 +43,75 @@ class Buku extends CI_Controller {
 
 	public function add()
 	{
-		$this->load->library('session');
-		if ($this->session->has_userdata('username')) {
-			$this->load->helper('url');
-			$this->load->view('master/header');
-			$this->load->view('master/navigation');
-			$this->load->view('pages/buku/addBuku');
-			$this->load->view('master/jsAdd');
-			$this->load->view('master/footer');
+		// create the data object
+	    $data = new stdClass();
+
+	    $this->load->helper('form');
+	    $this->load->library('form_validation');
+
+	    // set validation rules
+	    $this->form_validation->set_rules('judul', 'Judul Buku', 'required');
+	    $this->form_validation->set_rules('tahun', 'Tahun Terbitan', 'required|min_length[4]');
+	    $this->form_validation->set_rules('pengarang', 'Nama Pengarang', 'required');
+	    $this->form_validation->set_rules('penerbit', 'Nama Penerbit', 'required');
+	    $this->form_validation->set_rules('harga', 'Harga', 'required');
+
+	    if ($this->form_validation->run() === false) {
+			$this->load->library('session');
+			if ($this->session->has_userdata('username')) {
+				$this->load->helper('url');
+				$this->load->view('master/header');
+				$this->load->view('master/navigation');
+				$this->load->view('pages/buku/addBuku');
+				$this->load->view('master/jsAdd');
+				$this->load->view('master/footer');
+			} else {
+				$this->load->helper('url');
+				header('location:'.base_url().'user/login');
+			}
 		} else {
-			$this->load->helper('url');
-			header('location:'.base_url().'user/login');
+			$judul = $this->input->post('judul');
+			$tahun = $this->input->post('tahun');
+			$pengarang = $this->input->post('pengarang');
+			$penerbit = $this->input->post('penerbit');
+			$harga = $this->input->post('harga');
+
+			if ($this->buku_model->create_buku($judul, $tahun, $pengarang, $penerbit, $harga)) {
+
+				$success = "creation success";
+				$data = array('success' => $success );
+
+				$this->load->library('session');
+				if ($this->session->has_userdata('username')) {
+					$this->load->helper('url');
+					$this->load->view('master/header');
+					$this->load->view('master/navigation');
+					$this->load->view('pages/buku/addBuku', $data);
+					$this->load->view('master/jsAdd');
+					$this->load->view('master/footer');
+				} else {
+					$this->load->helper('url');
+					header('location:'.base_url().'user/login');
+				}
+
+			} else {
+
+        		// user creation failed, this should never happen
+				$data->error = 'There was a problem creating new buku. Please try again.';
+
+				$this->load->library('session');
+				if ($this->session->has_userdata('username')) {
+					$this->load->helper('url');
+					$this->load->view('master/header');
+					$this->load->view('master/navigation');
+					$this->load->view('pages/buku/addBuku', $data);
+					$this->load->view('master/jsAdd');
+					$this->load->view('master/footer');
+				} else {
+					$this->load->helper('url');
+					header('location:'.base_url().'user/login');
+				}
+			}
 		}
 	}
 }
